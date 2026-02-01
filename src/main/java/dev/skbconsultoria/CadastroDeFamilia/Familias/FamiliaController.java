fd10552e57db8c820cae53a99dc15e261426c8ad
gskubs <guilherme.skubs@gmail.com>
2026-02-01 12:03:41 -0300

    chore: .gitignore inclui o ./data

--- src/main/java/dev/skbconsultoria/CadastroDeFamilia/Familias/FamiliaController.java
+++ src/main/java/dev/skbconsultoria/CadastroDeFamilia/Familias/FamiliaController.java
@@ -7,10 +7,48 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping
 public class FamiliaController {
 
+
+
+
+
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Bem vindo, essa é a mensagem inicial";
     }
+
+    //VAMOS COMEÇAR NO BASICO NO CRUD
+
+    //Adicionar familiar (CREATE) posMapping para adicionar
+    @PostMapping("/criar")
+    public String criarFamilia(){
+        return "Familiar criado com sucesso";
+    }
+
+    //MOSTRAR TODOS OS FAMILIARES (READ) @GetMapping é usado para mostrar um resultado
+    @GetMapping("/todos")
+    public String mostrarFamilia(){
+        return "Essa é minha familia";
+    }
+
+    // MOSTRAR familiares por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarFamiliaPorId(){
+        return "Essa é minha familia por id";
+    }
+
+    //Alterar dados dos familiares (UPDATE) @PutMapping usado para alterar dados
+    @PutMapping("/alterarID")
+    public String alterarFamiliarPorId(){
+        return "Alterar familiar por id";
+    }
+
+    // Deletar familiar (DELETE) @DeleteMapping usado para deletar dados
+    @DeleteMapping("/deletarID")
+    public String deletarFamiliaPorId(){
+        return "Deletando familiar por id";
+    }
+
+
 
 
 
