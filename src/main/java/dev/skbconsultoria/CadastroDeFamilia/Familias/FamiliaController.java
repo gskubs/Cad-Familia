9fadfc23ba7d3d678abfd73325683992704a1e0b
gskubs <guilherme.skubs@gmail.com>
2026-02-04 03:17:17 -0300

    refactor: Refatora o controller para incluir o responseEntity nas minhas respostas de requisições do cliente

--- src/main/java/dev/skbconsultoria/CadastroDeFamilia/Familias/FamiliaController.java
+++ src/main/java/dev/skbconsultoria/CadastroDeFamilia/Familias/FamiliaController.java
@@ -1,4 +1,6 @@
 package dev.skbconsultoria.CadastroDeFamilia.Familias;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,41 +22,63 @@ public class FamiliaController {
 
     //Adicionar familiar (CREATE) @PostMapping para adicionar ao BD dados
     @PostMapping("/criar")
-    public FamiliaDTO criarFamilia(@RequestBody FamiliaDTO familiaDTO){
-        return familiaService.criarFamilia(familiaDTO);
+    public ResponseEntity<String> criarFamilia(@RequestBody FamiliaDTO familia){
+        FamiliaDTO novoFamiliar = familiaService.criarFamilia(familia);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Familiar adicionado com sucesso: " + novoFamiliar.getNome() + " (ID): "  + novoFamiliar.getId());
     }
 
     //MOSTRAR TODOS OS FAMILIARES (READ) @GetMapping é usado para mostrar do BD um resultado
     @GetMapping("/todos")
-    public List<FamiliaDTO> listarFamilia(){
-        return familiaService.listarFamilia();
+    public ResponseEntity <List<FamiliaDTO>> listarFamilia(){
+        List<FamiliaDTO> listaFamilia = familiaService.listarFamilia();
+        return ResponseEntity.ok(listaFamilia);
     }
 
     // MOSTRAR familiares por ID (READ) é aqui que o user vai preencher o ID que ele quer buscar
+    //Colocar o <?> para puxar generics ou seja todos os tipos de dados para puxar todos os tipos de dados
     @GetMapping("/todos/{id}")
-    public FamiliaDTO listarFamiliaID(@PathVariable Long id){
-       return familiaService.listarFamiliaID(id);
-           }
+    public ResponseEntity<?> listarFamiliaID(@PathVariable Long id){
+        FamiliaDTO familia = familiaService.listarFamiliaID(id);
+        if(familia != null){
+            return ResponseEntity.ok(familia);
+        }
+        else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Familiar: " + id + " Não existe nos meus registros");
+        }
+    }
 
     //Alterar dados dos familiares (UPDATE) @PutMapping usado para alterar dados
     //como o @PutMapping é uma mistura do PostMapping com o GetMapping, precicamos colocar a variavel
     //@PathVariable para buscar dados nesse caso por ID e o RequestBody, pois precisamo buscar os dados que precisam ser alterados
     @PutMapping("/alterar/{id}")
-    public FamiliaDTO alterarFamiliarPorId(@PathVariable Long id, @RequestBody FamiliaDTO familiaAtualizado){
-        return familiaService.atualizarFamilia(id, familiaAtualizado);//Dessa maneira eu consigo fazer o update
+    public ResponseEntity<?> alterarFamiliarPorId(@PathVariable Long id, @RequestBody FamiliaDTO familiaAtualizado){
+        FamiliaDTO familia = familiaService.atualizarFamilia(id, familiaAtualizado);
+        if(familia != null){
+            return ResponseEntity.ok(familia);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Nenhum Familiar com esse ID " + id + " foi encontrado!");
+        }
+
     }
 
     // Deletar familiar (DELETE) @DeleteMapping usado para deletar dados
     //Usar o @PathVariable sempre que precisa ser passado um dado para buscar o dado
     @DeleteMapping("/deletar/{id}")
-    public void deletarFamiliarPorId(@PathVariable Long id){
-        familiaService.deletarFamiliarPorId(id);
+    public ResponseEntity<String> deletarFamiliarPorId(@PathVariable Long id){
+
+        if (familiaService.listarFamiliaID(id) != null) {
+            familiaService.deletarFamiliarPorId(id);
+            return ResponseEntity.ok("Familiar com ID " + id + " Deletado com Sucesso!!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Nenhum Familiar com esse ID" + id + " foi encontrado!");
+        }
+
     }
-
-
-
-
-
-
 
 }
